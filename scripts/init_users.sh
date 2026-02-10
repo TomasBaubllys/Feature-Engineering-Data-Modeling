e7954964d4425ced6f58dde0ffe17edc29fa549d
Tomas <tomas.baublys@mif.stud.vu.lt>
2026-02-10 16:28:31 +0200

    user creation script upgrade

--- scripts/init_users.sh
+++ scripts/init_users.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo create_sudo_user.sh "itns" "8989IEO1"
-sudo create_sudo_user.sh "kumba" "kumba"
+sudo ./create_sudo_user.sh "itns" "8989IEO1"
+sudo ./create_sudo_user.sh "kumba" "kumba"
